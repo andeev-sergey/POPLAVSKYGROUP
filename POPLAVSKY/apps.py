31eb7b4bf92f5f0de22d2b25b8a4e554dee273d8
andreev-sergey <andreevsergeyvrn@gmail.com>
2020-07-30 15:35:13 +0300

    Start Project
    Signed-off-by: andreev-sergey <andreevsergeyvrn@gmail.com>

---
+++ POPLAVSKY/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PoplavskyConfig(AppConfig):
+    name = 'POPLAVSKY'
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andreev-sergey/Projects/POPLAVSKYGROUP/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
