10ea95f4beeb28accb8ecc0c712123a13a3ea3b4
andreev-sergey <andreevsergeyvrn@gmail.com>
2020-08-14 17:15:48 +0300

    Case fields
    Signed-off-by: andreev-sergey <andreevsergeyvrn@gmail.com>

--- POPLAVSKY/models.py
+++ POPLAVSKY/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+import datetime
+
+
+def year_choices():
+    return [(r, r) for r in range(1984, datetime.date.today().year + 1)]
+
+
+def current_year():
+    return datetime.date.today().year
 
 
 # Create your models here.
@@ -27,12 +36,26 @@ class CaseCategory(models.Model):
         verbose_name_plural = 'Категории элементов портфолио'
 
 
+class Areas(models.Model):
+    name_ru = models.CharField('Название зоны', max_length=30)
+    name_eng = models.CharField('Название зоны на английском', max_length=30)
+    area_value = models.CharField('Значение в м2')
+
+    def __str__(self):
+        return self.area_value
+
 class Case(models.Model):
     category = models.ForeignKey('Категория', CaseCategory, on_delete=models.CASCADE)
     preview_image = models.ImageField("Превью", upload_to='Cases/', blank=True)
     name_ru = models.CharField('Название проекта', max_length=30)
     name_eng = models.CharField('Название проекта на английском', max_length=30)
-    descrition_ru = models.CharField('Описание проекта', max_length=30)
-    descrition_eng = models.CharField('Описание проекта на английском', max_length=30)
+    short_descrition_ru = models.CharField('Краткое писание проекта', max_length=30)
+    short_descrition_eng = models.CharField('Краткое описание проекта на английском', max_length=30)
+    descrition_ru = models.TextField('Описание проекта', max_length=500)
+    descrition_eng = models.TextField('Описание проекта на английском', max_length=500)
     price_rub = models.IntegerField('Цена в рублях', blank=False)
     price_usd = models.IntegerField('Цена в рублях', blank=False)
+    Style_ru = models.CharField('Стиль проекта', max_length=30)
+    style_eng = models.CharField('Стиль проекта на английском', max_length=30)
+    year = models.IntegerField('Год проекта', choices=year_choices, default=current_year())
+    areas = models.ManyToManyField('Участки', Areas, blank=False)
