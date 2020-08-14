385abfa0ff14616d03f3976cdf55afc646b34e88
andreev-sergey <andreevsergeyvrn@gmail.com>
2020-08-14 17:30:49 +0300

    Reviews & Case fixes
    Signed-off-by: andreev-sergey <andreevsergeyvrn@gmail.com>

--- POPLAVSKY/models.py
+++ POPLAVSKY/models.py
@@ -36,13 +36,22 @@ class CaseCategory(models.Model):
         verbose_name_plural = 'Категории элементов портфолио'
 
 
-class Areas(models.Model):
+class Area(models.Model):
     name_ru = models.CharField('Название зоны', max_length=30)
     name_eng = models.CharField('Название зоны на английском', max_length=30)
     area_value = models.CharField('Значение в м2')
 
     def __str__(self):
         return self.area_value
+
+
+class Room(models.Model):
+    name_ru = models.CharField('Название комнаты', max_length=30)
+    name_eng = models.CharField('Название комнаты на английском', max_length=30)
+    image = models.ImageField('Изображение', upload_to='Rooms/', blank=True)
+    room_descrition_ru = models.TextField('Описание комнаты', max_length=500)
+    room_descrition_eng = models.TextField('Описание комнаты на английском', max_length=500)
+
 
 class Case(models.Model):
     category = models.ForeignKey('Категория', CaseCategory, on_delete=models.CASCADE)
@@ -58,4 +67,25 @@ class Case(models.Model):
     Style_ru = models.CharField('Стиль проекта', max_length=30)
     style_eng = models.CharField('Стиль проекта на английском', max_length=30)
     year = models.IntegerField('Год проекта', choices=year_choices, default=current_year())
-    areas = models.ManyToManyField('Участки', Areas, blank=False)
+    areas = models.ManyToManyField(Area, verbose_name='Площади', blank=False)
+    project_plan = models.ImageField("План проекта", upload_to='Cases/', blank=True)
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+
+
+class Review(models.Model):
+    name = models.CharField('Имя', blank=False, max_length=30)
+    title_ru = models.CharField('Должность', max_length=30)
+    title_eng = models.CharField('Должность на английском', max_length=30)
+    image = models.ImageField("Фото клиента", upload_to='Clients/', blank=True)
+    project = models.ForeignKey('Проект', Case, blank=True)
+    review_title_ru = models.CharField('Заголовок отзыва', blank=False, max_length=30)
+    review_body_ru = models.TextField('Отзыв', blank=False)
+    review_title_eng = models.CharField('Заголовок отзыва на англипйском', blank=False, max_length=30)
+    review_body_eng = models.TextField('Отзыв на английском', blank=False)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
